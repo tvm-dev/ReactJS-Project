ee5e408858d3565e277c60883562ee7b1fc22c60
thiago viana <tvm.dev2015@gmail.com>
2021-11-24 12:36:35 -0300

    Db Json, select from BD, etc

--- src/components/pages/Project/ProjectForm/index.js
+++ src/components/pages/Project/ProjectForm/index.js
@@ -2,15 +2,34 @@ import Input from '../../Form/Input'
 import Select from '../../Form/Select'
 import SubmitButton from '../../Form/SubmitButton'
 import styles from './ProjectForm.module.css'
+import { useEffect, useState } from 'react'
 
 
-function ProjectForm({btnText}){
+function ProjectForm({ btnText }){
+  const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+      fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+       },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err) => console.log(err))
+    }, [])
+
+
+
     return (
        <form className={styles.form}>
 
             <Input type="text" text="Project name" name="name" placeholder="Insert the Project name" />
             <Input type="number" text="Budget Project" name="budget" placeholder="Insert the total Budget" />
-            <Select name="category_id" text="Select one Category" />
+            <Select name="category_id" text="Select one Category" options={categories} />
             <SubmitButton text={btnText} />
       </form>
     )
